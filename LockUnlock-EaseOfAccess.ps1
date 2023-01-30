e8b43c45cdd85537140ec1e4dfe2d9056e23a7bc
A-seirved <123914494+A-seirved@users.noreply.github.com>
2023-01-30 15:41:23 +1100

    Update LockUnlock-EaseOfAccess.ps1

--- LockUnlock-EaseOfAccess.ps1
+++ LockUnlock-EaseOfAccess.ps1
@@ -1,49 +1,35 @@
-$ControlPanelKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
-$SettingsKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$ControlPanelKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Control Panel\Settings"
+$WindowsSettingsKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Settings"
 
-Function EnableSettings {
-    If (!(Test-Path $ControlPanelKey)) {
-        New-Item -Path $ControlPanelKey -Force | Out-Null
-        New-ItemProperty -Path $ControlPanelKey -Name NoControlPanel -PropertyType DWORD -Value 0 -Force | Out-Null
-    }
-    If (!(Test-Path $SettingsKey)) {
-        New-Item -Path $SettingsKey -Force | Out-Null
-        New-ItemProperty -Path $SettingsKey -Name NoWindowsSettings -PropertyType DWORD -Value 0 -Force | Out-Null
-    }
-    Set-ItemProperty -Path $ControlPanelKey -Name NoControlPanel -Value 1
-    Set-ItemProperty -Path $SettingsKey -Name NoWindowsSettings -Value 1
-    Set-ItemProperty -Path $ControlPanelKey -Name NoClose -PropertyType DWORD -Value 0 -Force | Out-Null
+If (!(Test-Path $ControlPanelKey)) {
+    New-Item -Path $ControlPanelKey -Force | Out-Null
+    New-ItemProperty -Path $ControlPanelKey -Name "NoControlPanel" -Value 1 -PropertyType DWord | Out-Null
 }
 
-Function DisableSettings {
-    If (!(Test-Path $ControlPanelKey)) {
-        New-Item -Path $ControlPanelKey -Force | Out-Null
-    }
-    If (!(Test-Path $SettingsKey)) {
-        New-Item -Path $SettingsKey -Force | Out-Null
-    }
-    Set-ItemProperty -Path $ControlPanelKey -Name NoControlPanel -Value 1
-    Set-ItemProperty -Path $SettingsKey -Name NoWindowsSettings -Value 1
-    Set-ItemProperty -Path $ControlPanelKey -Name NoClose -PropertyType DWORD -Value 1 -Force | Out-Null
+If (!(Test-Path $WindowsSettingsKey)) {
+    New-Item -Path $WindowsSettingsKey -Force | Out-Null
+    New-ItemProperty -Path $WindowsSettingsKey -Name "NoWindowsSettings" -Value 1 -PropertyType DWord | Out-Null
 }
 
-Function ResyncSettings {
-    $ControlPanelSetting = Get-ItemProperty -Path $ControlPanelKey -Name NoControlPanel
-    $SettingsSetting = Get-ItemProperty -Path $SettingsKey -Name NoWindowsSettings
-    If ($ControlPanelSetting.NoControlPanel -ne $SettingsSetting.NoWindowsSettings) {
-        If ($ControlPanelSetting.NoControlPanel -eq 1) {
-            Set-ItemProperty -Path $SettingsKey -Name NoWindowsSettings -Value 1
-        }
-        Else {
-            Set-ItemProperty -Path $ControlPanelKey -Name NoControlPanel -Value 0
-        }
-    }
-}
+$ControlPanelValue = Get-ItemPropertyValue -Path $ControlPanelKey -Name "NoControlPanel"
+$WindowsSettingsValue = Get-ItemPropertyValue -Path $WindowsSettingsKey -Name "NoWindowsSettings"
 
-If (!(Test-Path $ControlPanelKey) -or !(Test-Path $SettingsKey)) {
-    EnableSettings
+If ($ControlPanelValue -eq $WindowsSettingsValue) {
+    If ($ControlPanelValue -ne 0) {
+        Set-ItemProperty -Path $ControlPanelKey -Name "NoControlPanel" -Value 0
+        Set-ItemProperty -Path $WindowsSettingsKey -Name "NoWindowsSettings" -Value 0
+    }
 }
 Else {
-    DisableSettings
-    ResyncSettings
+    $ControlPanelValue = Get-ItemPropertyValue -Path $ControlPanelKey -Name "NoControlPanel"
+    $WindowsSettingsValue = Get-ItemPropertyValue -Path $WindowsSettingsKey -Name "NoWindowsSettings"
+
+    If ($ControlPanelValue -ne $WindowsSettingsValue) {
+        If ($ControlPanelValue -ne 0) {
+            Set-ItemProperty -Path $ControlPanelKey -Name "NoControlPanel" -Value $WindowsSettingsValue
+        }
+        If ($WindowsSettingsValue -ne 0) {
+            Set-ItemProperty -Path $WindowsSettingsKey -Name "NoWindowsSettings" -Value $ControlPanelValue
+        }
+    }
 }
