1c6368ce7a7957fc20cd103ad668934471ba6a62
A-seirved <123914494+A-seirved@users.noreply.github.com>
2023-01-30 14:25:59 +1100

    Update LockUnlock-EaseOfAccess.ps1

--- LockUnlock-EaseOfAccess.ps1
+++ LockUnlock-EaseOfAccess.ps1
@@ -1,18 +1,14 @@
 # Check if the Control Panel is currently disabled
-$isControlPanelDisabled = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Control Panel" -Name "NoControlPanel"
+$isControlPanelDisabled = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoControlPanel"
 
 # If the Control Panel is currently disabled, re-enable it
-if ($isControlPanelDisabled) {
-  Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Control Panel" -Name "NoControlPanel"
-  Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden"
-  Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_ShowControlPanel"
+if ($isControlPanelDisabled.NoControlPanel -eq 1) {
+  Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoControlPanel"
   Write-Host "Control Panel has been re-enabled."
 }
 # If the Control Panel is not currently disabled, disable it except for the Ease of Access settings
 else {
-  New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Control Panel" -Value 1 -PropertyType DWORD
-  New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 1 -PropertyType DWORD
-  New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_ShowControlPanel" -Value 0 -PropertyType DWORD
+  New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoControlPanel" -Value 1 -PropertyType DWORD
   New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Control Panel\Accessibility" -Name "NoControlPanel" -Value 0 -PropertyType DWORD
   Write-Host "Control Panel has been disabled, except for the Ease of Access settings."
 }
