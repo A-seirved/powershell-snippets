8b29653a4045d06593b5438349cacbd0fb9ecab9
A-seirved <123914494+A-seirved@users.noreply.github.com>
2023-01-30 14:48:23 +1100

    Update LockUnlock-EaseOfAccess.ps1

--- LockUnlock-EaseOfAccess.ps1
+++ LockUnlock-EaseOfAccess.ps1
@@ -7,40 +7,45 @@ Function Check-SettingsState {
   return ($isControlPanelDisabled.NoControlPanel, $isWindowsSettingsDisabled.NoWindowsSettings)
 }
 
-# Function to disable both the Control Panel and Windows Settings
+# Function to disable the Control Panel and Windows Settings, except for the Ease of Access settings
 Function Disable-Settings {
   New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoControlPanel" -Value 1 -PropertyType DWORD
   New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Control Panel\Accessibility" -Name "NoControlPanel" -Value 0 -PropertyType DWORD
   New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoWindowsSettings" -Value 1 -PropertyType DWORD
   New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Settings\Accessibility" -Name "NoWindowsSettings" -Value 0 -PropertyType DWORD
-  Write-Host "Both the Control Panel and Windows Settings have been disabled, except for the Ease of Access settings."
+  Write-Host "The Control Panel and Windows Settings have been disabled, except for the Ease of Access settings."
 }
 
-# Function to enable both the Control Panel and Windows Settings
+# Function to enable the Control Panel and Windows Settings
 Function Enable-Settings {
   Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoControlPanel"
   Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoWindowsSettings"
-  Write-Host "Both the Control Panel and Windows Settings have been re-enabled."
+  Write-Host "The Control Panel and Windows Settings have been re-enabled."
+}
+
+# Function to synchronize the Control Panel and Windows Settings state
+Function Synchronize-Settings {
+  $controlPanelState, $windowsSettingsState = Check-SettingsState
+  if ($controlPanelState -ne $windowsSettingsState) {
+    if ($controlPanelState -eq 1) {
+      Disable-Settings
+    } else {
+      Enable-Settings
+    }
+  }
 }
 
 # Get the current state of the Control Panel and Windows Settings
 $controlPanelState, $windowsSettingsState = Check-SettingsState
 
-# If both the Control Panel and Windows Settings are disabled, enable them
-if ($controlPanelState -eq 1 -and $windowsSettingsState -eq 1) {
-  Enable-Settings
-}
-# If both the Control Panel and Windows Settings are enabled, disable them
-elseif ($controlPanelState -eq 0 -and $windowsSettingsState -eq 0) {
+# If the Control Panel and Windows Settings are enabled, disable them
+if ($controlPanelState -eq 0 -and $windowsSettingsState -eq 0) {
   Disable-Settings
 }
-# If the Control Panel and Windows Settings are unsynchronized, resynchronize them by disabling or enabling them both
+# If the Control Panel and Windows Settings are disabled, enable them
 else {
-  Write-Host "The Control Panel and Windows Settings are unsynchronized."
-  if ($controlPanelState -eq 1) {
-    Disable-Settings
-  }
-  else {
-    Enable-Settings
-  }
+  Enable-Settings
 }
+
+# Synchronize the Control Panel and Windows Settings state
+Synchronize-Settings
